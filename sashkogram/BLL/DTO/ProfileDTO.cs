15e5a47f6cba8d2733aca24715c48a6a9f442c81
Lipovets <lipavets@yahoo.com.ua>
2016-07-13 16:15:58 +0300

    second commit

---
+++ sashkogram/BLL/DTO/ProfileDTO.cs
@@ -0,0 +1,27 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class ProfileDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfileStatus { get; set; }
+
+        public ICollection<PhotoDTO> Avatars { get; set; }
+        public ICollection<PhotoDTO> ProfilePhotos { get; set; }
+
+        public ProfileDTO()
+        {
+            ProfilePhotos = new List<PhotoDTO>();
+            Avatars = new List<PhotoDTO>();
+        }
+        //public List<ProfileDTO> Followers { get; set; }
+        //public List<ProfileDTO> Following { get; set; }
+    }
+}
